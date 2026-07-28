fb2b270042b6cadd6f5095519b81c460ee0c48d3
Sam <samuel.arellano@cs.inc>
2026-07-28 09:44:54 -0700

    Initial commit: CMake setup and SDL2 window

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <SDL2/SDL.h>
+#include <cstdio>
+
+constexpr int CHIP8_WIDTH  = 64;
+constexpr int CHIP8_HEIGHT = 32;
+constexpr int SCALE        = 15;   // 64x32 is tiny; scale it up
+
+int main(int argc, char** argv) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "CHIP-8",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        CHIP8_WIDTH * SCALE, CHIP8_HEIGHT * SCALE,
+        SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        std::fprintf(stderr, "CreateWindow failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer =
+        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) running = false;
+            if (event.type == SDL_KEYDOWN &&
+                event.key.keysym.sym == SDLK_ESCAPE) running = false;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
